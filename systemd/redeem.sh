2574eda305d3fe530c17c180d89f88c135b8457b
Elias <elias@iagent.no>
2013-11-26 02:39:07 +0100

    More namechanges

--- systemd/redeem.sh
+++ systemd/redeem.sh
@@ -3,13 +3,13 @@
 case $1 in 
 	start)
 		export LD_LIBRARY_PATH=/usr/local/lib
-		cd /opt/Replicape/software/ && python Replicape.py
-		echo "Replicape started ok"
+		cd /opt/Replicape/software/ && python Redeem.py
+		echo "Redeem started ok"
 		exit 0
 		;;
 	stop)
-		pkill -9 -f Replicape.py
-		echo "Execed killall replicape"
+		pkill -9 -f Redeem.py
+		echo "Execed killall Redeem"
 		;;
 	*)
 		echo "Usage: $0 {start|stop}"
