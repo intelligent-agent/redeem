Fixed delays computation for stepper motor in PRU0

software/Pru.py
@@ -39,12 +39,9 @@ class Pru:
     ddr_lock = Lock()
 
     def __init__(self, firmware):
-        pru_hz 			    = 200*1000*1000             # The PRU has a speed of 200 MHz
-        self.s_pr_inst      = (1.0/pru_hz)          # I take it every instruction is a single cycle instruction
-        self.inst_pr_loop 	= 0                        # This is the minimum number of instructions needed to step.  It is already substracted into the PRU
-        self.inst_pr_delay 	= 2                         # Every loop adds two instructions: i-- and i != 0            
-        self.sec_to_inst_dev = (self.s_pr_inst*2)
-        self.max_delay_cycles = 4/self.inst_pr_delay*pru_hz     #Maximum delay to avoid bugs
+        self.pru_hz		    = 200*1000*1000             # The PRU has a speed of 200 MHz
+        self.s_pr_inst      = (1.0/self.pru_hz)          # I take it every instruction is a single cycle instruction
+        self.max_delay_cycles = self.pru_hz*4           #Maximum delay to avoid bugs (4 seconds)
         self.pru_data       = []      	    	        # This holds all data for one move (x,y,z,e1,e2)
         self.ddr_used       = Queue.Queue()             # List of data lengths currently in DDR for execution
         self.ddr_reserved   = 0      
@@ -108,8 +105,10 @@ class Pru:
     def add_data(self, data):
         """ Add some data to one of the PRUs """
         (pins, dirs, options, delays) = data                       	    # Get the data
-        delays = np.clip(((np.array(delays)/self.s_pr_inst)-self.inst_pr_loop), 1, self.max_delay_cycles)
+        delays = np.clip(np.array(delays)/self.s_pr_inst, 1, self.max_delay_cycles)
         data = np.array([pins,dirs,options, delays.astype(int)])		        	    # Make a 2D matrix combining the ticks and delays
+
+
         self.pru_data = data.transpose()   
 
     def has_capacity_for(self, data_len):