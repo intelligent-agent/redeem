2cfbee46b0fea577891988b66216542a8fefa882
Mathieu Monney <zittix@xwaves.net>
2014-04-02 21:37:45 +0200

    Added M13{0,1,2} commands for adjusting PID settings

--- software/gcodes/M130_M131_M132.py
+++ software/gcodes/M130_M131_M132.py
@@ -14,8 +14,14 @@ from GCodeCommand import GCodeCommand
 class M130(GCodeCommand):
 
     def execute(self,g):
-       #FIXME: TODO
-       pass
+      extr = g.get_int_by_letter("P", 0)               
+      value = float(g.get_int_by_letter("S", 0))
+      if extr == 0:
+        self.printer.heaters['E'].set_p_value(value)
+      elif extr == 1:
+        self.printer.heaters['H'].set_p_value(value)
+      elif extr == 2:
+        self.printer.heaters['HBP'].set_p_value(value)
 
     def get_description(self):
         return "Set PID P-value, Format (M130 P0 S8.0)"
@@ -23,8 +29,14 @@ class M130(GCodeCommand):
 class M131(GCodeCommand):
 
     def execute(self,g):
-       #FIXME: TODO
-       pass
+      extr = g.get_int_by_letter("P", 0)               
+      value = float(g.get_int_by_letter("S", 0))
+      if extr == 0:
+        self.printer.heaters['E'].set_i_value(value)
+      elif extr == 1:
+        self.printer.heaters['H'].set_i_value(value)
+      elif extr == 2:
+        self.printer.heaters['HBP'].set_i_value(value)
 
     def get_description(self):
         return "Set PID I-value, Format (M131 P0 S8.0)"
@@ -32,8 +44,14 @@ class M131(GCodeCommand):
 class M132(GCodeCommand):
 
     def execute(self,g):
-       #FIXME: TODO
-       pass
+      extr = g.get_int_by_letter("P", 0)               
+      value = float(g.get_int_by_letter("S", 0))
+      if extr == 0:
+        self.printer.heaters['E'].set_d_value(value)
+      elif extr == 1:
+        self.printer.heaters['H'].set_d_value(value)
+      elif extr == 2:
+        self.printer.heaters['HBP'].set_d_value(value)
 
     def get_description(self):
         return "Set PID D-value, Format (M132 P0 S8.0)"
