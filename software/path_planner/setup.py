7732bad79d36f48d87cf4d2d76229630a7b63db0
Mathieu Monney <zittix@xwaves.net>
2014-06-22 14:11:38 +0200

    Cleaning up path planner package due to last commit.

--- software/path_planner/setup.py
+++ software/path_planner/setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup, Extension
 
-module1 = Extension('_PathPlannerNative', sources = ['PathPlannerNative.i', 'PathPlanner.cpp','PruTimer.cpp','prussdrv.c','Logger.cpp'],  swig_opts=['-c++','-builtin'], extra_compile_args = ['-std=c++0x','-g','-Ofast','-fpermissive','-D_GLIBCXX_USE_NANOSLEEP'])
+pathplanner = Extension('_PathPlannerNative', sources = ['PathPlannerNative.i', 'PathPlanner.cpp','PruTimer.cpp','prussdrv.c','Logger.cpp'],  swig_opts=['-c++','-builtin'], extra_compile_args = ['-std=c++0x','-g','-Ofast','-fpermissive','-D_GLIBCXX_USE_NANOSLEEP'])
 
 setup(name='PathPlannerNative',
       version='1.0',
@@ -10,5 +10,5 @@ setup(name='PathPlannerNative',
       author='Mathieu Monney',
       author_email='zittix@xwaves.net',
       url='http://www.xwaves.net',
-      ext_modules = [module1],
+      ext_modules = [pathplanner],
      )
