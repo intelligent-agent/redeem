6f8dd5169ebae53c5dee5051f85fb2042d1fd5e0
Elias Bakken <elias@iagent.no>
2014-07-29 13:30:14 +0200

    Changed the lisence to GPLv3 to be compatible with Repetier firmware lisence

--- software/Delta.py
+++ software/Delta.py
@@ -1,4 +1,25 @@
-# Delta.py
+'''
+Author: Elias Bakken
+email: elias(dot)bakken(at)gmail(dot)com
+Website: http://www.thing-printer.com
+License: GNU GPL v3: http://www.gnu.org/copyleft/gpl.html
+
+ This work in this file has been heavily influenced by the work of Steve Graves
+ from his document on Delta printer kinematics: https://groups.google.com/forum/#!topic/deltabot/V6ATBdT43eU
+ 
+ Redeem is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Redeem is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Redeem.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 # Helper functions for kinematics for Delta printers
 import numpy as np                                                          # Needed for sqrt
--- software/Printer.py
+++ software/Printer.py
@@ -4,7 +4,20 @@ Printer class holding all printer components
 Author: Mathieu Monney
 email: zittix(at)xwaves(dot)net
 Website: http://www.xwaves.net
-License: CC BY-SA: http://creativecommons.org/licenses/by-sa/2.0/
+License: GNU GPL v3: http://www.gnu.org/copyleft/gpl.html
+
+ Redeem is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Redeem is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Redeem.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from Path import Path
