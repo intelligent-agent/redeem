a7086e2e2bb00cc2dcb3592add738962df97631c
zarthrag <anthony.clay@zarthcode.com>
2015-01-23 20:30:31 -0600

    Logging cleanup.

--- redeem/gcodes/M400.py
+++ redeem/gcodes/M400.py
@@ -15,20 +15,16 @@ import logging
 class M400(GCodeCommand):
 
     def execute(self, g):
-        logging.info("M400 - Waiting until done")
 
         # This needs to be a standard method somewhere
         if not self.printer.path_planner.queue_sync_event(False):    # No blocking of the PRU, (notification only)
-            logging.info("M400 - Sync Event Queue Failed - waiting...")
             self.path_planner.wait_until_done()                      # The move buffer is already empty! fallback to this to ensure we're in sync.
             self.printer.sync_commands.task_done()                   # We should be at the front of the line.
             self.on_sync()                                           # Complete execution
-        logging.info("M400 - Sync Event Queue Successful - waiting...")
 
     def on_sync(self, g):
         # self.printer.path_planner.clear_sync_event()  # Only needed if blocking the PRU
         self.readyEvent.set()                           # This is REQUIRED for synchronous commands!
-        logging.info("M400 - Sync done")
 
     def get_description(self):
         return "Wait until all buffered paths are executed"
