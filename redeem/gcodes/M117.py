45dc70bca0b886ea37fb1d0b3accbc5c92ba3282
Elias Bakken <elias@iagent.no>
2016-03-26 18:23:59 +0100

    M117 sends command

--- redeem/gcodes/M117.py
+++ redeem/gcodes/M117.py
@@ -10,12 +10,17 @@ License: GNU GPL v3: http://www.gnu.org/copyleft/gpl.html
 
 from GCodeCommand import GCodeCommand
 
+try:
+    from Alarm import Alarm
+except ImportError:
+    from redeem.Alarm import Alarm
 
 class M117(GCodeCommand):
     def execute(self, g):
         # This G-code can be used directly
         text = g.message.strip("M117 ")
-        self.printer.comms["toggle"].send_message(text)
+        Alarm.action_command("display_message", text)
+        #self.printer.comms["octoprint"].send_message(text)
 
     def get_description(self):
         return "Send a message to a connected display"
