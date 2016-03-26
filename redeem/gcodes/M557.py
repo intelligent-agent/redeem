74c146022278caf970494bc3b46fad47d0621260
Matti Airas <mairas@iki.fi>
2016-03-26 23:05:28 +0200

    M557: wrap docstring lines

--- redeem/gcodes/M557.py
+++ redeem/gcodes/M557.py
@@ -2,7 +2,12 @@
 GCode M557 
 Example: M557 P1 X30 Y40.5
 
-Set the points at which the bed will be probed to compensate for its plane being slightly out of horizontal. The P value is the index of the point (indices start at 0) and the X and Y values are the position to move extruder 0 to to probe the bed. An implementation should allow a minimum of three points (P0, P1 and P2). This just records the point coordinates; it does not actually do the probing. See G32. 
+Set the points at which the bed will be probed to compensate for its plane
+being slightly out of horizontal. The P value is the index of the point
+(indices start at 0) and the X and Y values are the position to move extruder 0
+to to probe the bed. An implementation should allow a minimum of three points
+(P0, P1 and P2). This just records the point coordinates; it does not actually
+do the probing. See G32.
 
 Author: Elias Bakken
 License: CC BY-SA: http://creativecommons.org/licenses/by-sa/2.0/
