ce83fdc870c88c258451542b3b5f945ab3823933
Elias Bakken <elias@iagent.no>
2016-05-22 19:26:20 +0200

    Bump rev

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ pathplanner = Extension(
 
 setup(
     name = "Redeem",
-    version = "1.2.1",
+    version = "1.2.3",
     packages = find_packages(exclude=["redeem/path_planner"]),
     data_files=[
         ('redeem/firmware', [
