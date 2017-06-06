d2f02fe3f772987b86ff9984e7d2b61b2e993c37
Andrew Wiley <wiley@outlook.com>
2017-06-06 09:01:27 +0000

    Fix M574 to operate under the new gcode parser
    The GCode parser was recently overhauled to make it more standards
    compliant, but our M574 gcode isn't actually standards compliant.
    Work around this and add a new syntax that's more compliant.

--- redeem/gcodes/M574.py
+++ redeem/gcodes/M574.py
@@ -5,6 +5,7 @@ License: CC BY-SA: http://creativecommons.org/licenses/by-sa/2.0/
 """
 
 from GCodeCommand import GCodeCommand
+from redeem.Printer import Printer
 import logging
 import os
 
@@ -12,10 +13,39 @@ class M574(GCodeCommand):
 
     def execute(self, g):
         tokens = g.get_tokens()
+        logging.debug("end stop tokens: "+str(tokens))
+
         if len(tokens) > 0:
             es = tokens[0]
-            config = tokens[1] if len(tokens) > 1 else ""
-
+            
+            config = ""
+            index = 1
+            while index < len(tokens):
+                if tokens[index][0] not in Printer.AXES:
+                    raise ValueError("Invalid endstop axis: "+tokens[index])
+                
+                if len(tokens[index]) > 1:
+                    # The axis has a number after it - use that
+                    value = float(tokens[index][1::])
+                    if value < 0:
+                        config += tokens[index][0].lower()+"_neg,"
+                    elif value > 0:
+                        config += tokens[index][0].lower()+"_pos,"
+                    else:
+                        raise ValueError("Non-directional endstops aren't supported... yet")
+                    index += 1
+                else:
+                    if len(tokens) >= index + 4:
+                        if ''.join(tokens[index + 1 : index + 4]).lower() == "neg":
+                            config += tokens[index][0].lower()+"_neg,"
+                        elif ''.join(tokens[index + 1 : index + 4]).lower() == "pos":
+                            config += tokens[index][0].lower()+"_pos,"
+                        else:
+                            raise ValueError("Can't parse endstop direction suffix: "+tokens[index + 1::])
+                        index += 4
+                    else:
+                        raise ValueError("Endstop direction suffix is too short: "+tokens[index + 1::])
+             
             if not es in self.printer.end_stops:
                 logging.warning("M574: Wrong end stop: "+str(es))
             
