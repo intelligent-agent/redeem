e6a0213d63c2528e8e93b2399e88c037e32c81f1
Bryan <gruvin@gmail.com>
2017-07-18 16:53:14 +1200

    Updated M114 test to check for use of mm=True

--- tests/gcode/test_M114.py
+++ tests/gcode/test_M114.py
@@ -17,7 +17,6 @@ class M112_Tests(MockPrinter):
         E = round(random()*200, 1)
         H = round(random()*200, 1)
         self.printer.path_planner.get_current_pos = mock.Mock(
-                spec=["xmm"],
                 return_value = {
                     'A': A, 
                     'C': C, 
@@ -31,6 +30,7 @@ class M112_Tests(MockPrinter):
             )
         g = Gcode({"message": "M114"})
         self.printer.processor.gcodes[g.gcode].execute(g)
+        self.printer.path_planner.get_current_pos.assert_called_with(mm=True)
         self.assertEqual(g.answer, 
                 "ok C: X:{:.1f} Y:{:.1f} Z:{:.1f} E:{:.1f} A:{:.1f} B:{:.1f} C:{:.1f} H:{:.1f}".format(
                     X, Y, Z, E, A, B, C, H
