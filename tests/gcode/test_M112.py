86b310121b1105294fac74e4c1530ab1223ae964
Bryan <gruvin@gmail.com>
2017-07-18 15:55:32 +1200

    Corrected M111 test method names. Added test for M112.

---
+++ tests/gcode/test_M112.py
@@ -0,0 +1,14 @@
+from MockPrinter import MockPrinter
+import mock
+from Gcode import Gcode
+from random import random
+import math
+import logging
+
+class M112_Tests(MockPrinter):
+
+    def test_gcodes_M112(self):
+        self.printer.path_planner.emergency_interrupt = mock.Mock(return_value=None)
+        self.execute_gcode("M112")
+        self.printer.path_planner.emergency_interrupt.assert_called()
+  
