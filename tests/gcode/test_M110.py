602a856fa2f62ec0652ed2f9a693ac750f5471bb
Bryan <gruvin@gmail.com>
2017-07-18 15:18:44 +1200

    Added test for M110

---
+++ tests/gcode/test_M110.py
@@ -0,0 +1,19 @@
+from MockPrinter import MockPrinter
+import mock
+from Gcode import Gcode
+from random import random
+import math
+
+class M110_Tests(MockPrinter):
+
+    def setUp(self):
+        Gcode.line_number = 99
+        
+    def test_gcodes_M110_no_param(self):
+        self.execute_gcode("M110")
+        self.assertEqual(Gcode.line_number, 0)
+        
+    def test_gcodes_M110_N123(self):
+        self.execute_gcode("M110 N123")
+        self.assertEqual(Gcode.line_number, 123)
+        
