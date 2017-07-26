f49b2c0c1a598a531a3bd0876f13edb1488fa784
Bryan <gruvin@gmail.com>
2017-07-26 13:17:59 +1200

    minor change to test_M13[012].py

--- tests/gcode/test_M130_131_132.py
+++ tests/gcode/test_M130_131_132.py
@@ -9,7 +9,7 @@ class M130_M131_M132_Tests(MockPrinter):
             self.printer.heaters[k].Kp = 1.0
             self.printer.heaters[k].Ti = 0.0
             self.printer.heaters[k].Td = 0.0
-        self.heaters = ["HBP", "E", "H", "A", "B", "C"]
+        self.heater_order = ["HBP", "E", "H", "A", "B", "C"]
 
     def test_gcodes_M130_no_args(self):
         g = self.execute_gcode("M130")
@@ -17,23 +17,23 @@ class M130_M131_M132_Tests(MockPrinter):
 
     def test_gcodes_M130_Pn_Sr(self):
         expected_Kp = {}
-        for k in self.heaters:
+        for k in self.heater_order:
             expected_Kp[k] = round(random(), 2)
-            self.execute_gcode("M130 P{} S{}".format(self.heaters.index(k)-1, expected_Kp[k]))
+            self.execute_gcode("M130 P{} S{}".format(self.heater_order.index(k)-1, expected_Kp[k]))
             self.assertEqual(self.printer.heaters[k].Kp, expected_Kp[k]) 
 
     def test_gcodes_M131_Pn_Sr(self):
         expected_Ti = {}
-        for k in self.heaters:
+        for k in self.heater_order:
             expected_Ti[k] = round(random(), 2)
-            self.execute_gcode("M131 P{} S{}".format(self.heaters.index(k)-1, expected_Ti[k]))
+            self.execute_gcode("M131 P{} S{}".format(self.heater_order.index(k)-1, expected_Ti[k]))
             self.assertEqual(self.printer.heaters[k].Ti, expected_Ti[k]) 
 
     def test_gcodes_M132_Pn_Sr(self):
         expected_Td = {}
-        for k in self.heaters:
+        for k in self.heater_order:
             expected_Td[k] = round(random(), 2)
-            self.execute_gcode("M132 P{} S{}".format(self.heaters.index(k)-1, expected_Td[k]))
+            self.execute_gcode("M132 P{} S{}".format(self.heater_order.index(k)-1, expected_Td[k]))
             self.assertEqual(self.printer.heaters[k].Td, expected_Td[k]) 
 
 
