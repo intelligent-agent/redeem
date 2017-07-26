f5ad042a5d09c3f8685f26bb9cde07515a6016c7
Bryan <gruvin@gmail.com>
2017-07-27 11:55:06 +1200

    Added test for M18. Changed Stepper::set_stepper_power_down() to a class method, since it affects all steppers.
    Removed unwanted code from test_M17.py

--- tests/gcode/test_M17.py
+++ tests/gcode/test_M17.py
@@ -8,9 +8,11 @@ class M17_Tests(MockPrinter):
         for name, stepper in self.printer.steppers.iteritems():
             if self.printer.config.getboolean('Steppers', 'in_use_' + name):
                 stepper.set_enabled = mock.Mock()
+        self.printer.path_planner.wait_until_done = mock.Mock()
 
     def test_gcodes_M17(self):
         self.execute_gcode("M17")
+        self.printer.path_planner.wait_until_done.assert_called()
         for name, stepper in self.printer.steppers.iteritems():
             if self.printer.config.getboolean('Steppers', 'in_use_' + name):
                 stepper.set_enabled.assert_called()
---
+++ tests/gcode/test_M18.py
@@ -0,0 +1,42 @@
+from MockPrinter import MockPrinter
+import mock
+from random import random
+from Stepper import Stepper
+
+class M17_Tests(MockPrinter):
+
+    def setUp(self):
+        for name, stepper in self.printer.steppers.iteritems():
+            if self.printer.config.getboolean('Steppers', 'in_use_' + name):
+                stepper.set_disabled = mock.Mock()
+        self.printer.path_planner.wait_until_done = mock.Mock()
+        Stepper.set_stepper_power_down = mock.Mock()
+
+    def test_gcodes_M18_no_args(self):
+        self.execute_gcode("M18")
+        self.printer.path_planner.wait_until_done.assert_called()
+        for name, stepper in self.printer.steppers.iteritems():
+            if self.printer.config.getboolean('Steppers', 'in_use_' + name):
+                stepper.set_disabled.assert_called()
+
+    def test_gcodes_M18_X_Y(self):
+        self.execute_gcode("M18 X Y")
+        self.printer.path_planner.wait_until_done.assert_called()
+        for name, stepper in self.printer.steppers.iteritems():
+            if self.printer.config.getboolean('Steppers', 'in_use_' + name):
+                if name in ["X", "Y"]:
+                    stepper.set_disabled.assert_called()
+                else:
+                    stepper.set_disabled.assert_not_called()
+
+    def test_gcodes_M18_D_no_arg(self):
+        self.execute_gcode("M18 D")
+        Stepper.set_stepper_power_down.assert_called_with(False)
+
+    def test_gcodes_M18_D1(self):
+        self.execute_gcode("M18 D0")
+        Stepper.set_stepper_power_down.assert_called_with(False)
+
+    def test_gcodes_M18_D1(self):
+        self.execute_gcode("M18 D1")
+        Stepper.set_stepper_power_down.assert_called_with(True)
