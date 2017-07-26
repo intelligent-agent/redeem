9180f8527e51203dc2471f680ff153292f108b11
Bryan <gruvin@gmail.com>
2017-07-26 13:15:13 +1200

    Added test for M130/1/2. Refactored redeem/gcodes/M130_M131_M132.py.

--- redeem/gcodes/M130_M131_M132.py
+++ redeem/gcodes/M130_M131_M132.py
@@ -13,10 +13,16 @@ from GCodeCommand import GCodeCommand
 
 class M130(GCodeCommand):
 
+    heaters = ["HBP", "E", "H", "A", "B", "C"]
+
+    @classmethod
+    def get_params(self, g):
+        self.extr = g.get_int_by_letter("P", 0) + 1
+        self.value = g.get_float_by_letter("S", 0.1)
+ 
     def execute(self, g):
-        extr = g.get_int_by_letter("P", 0)
-        value = g.get_float_by_letter("S", 0.1)
-        self.printer.heaters[["HBP", "E", "H"][extr+1]].Kp = value
+        self.get_params(g)
+        self.printer.heaters[M130.heaters[self.extr]].Kp = self.value
         
     def get_description(self):
         return "Set PID P-value, Format (M130 P0 S8.0)"
@@ -25,17 +31,11 @@ class M130(GCodeCommand):
         return "Set PID P-value, Format (M130 P0 S8.0), S<-1, 0, 1>"
 
 
-class M131(GCodeCommand):
+class M131(M130):
 
     def execute(self, g):
-        extr = g.get_int_by_letter("P", 0)
-        value = g.get_float_by_letter("S")
-        if extr == 0:
-            self.printer.heaters['E'].Ti = value
-        elif extr == 1:
-            self.printer.heaters['H'].Ti = value
-        elif extr == 2:
-            self.printer.heaters['HBP'].Ti = value
+        self.get_params(g)
+        self.printer.heaters[M131.heaters[self.extr]].Ti = self.value
 
     def get_description(self):
         return "Set PID I-value, Format (M131 P0 S8.0)"
@@ -44,17 +44,10 @@ class M131(GCodeCommand):
         return "Set PID I-value, Format (M131 P0 S8.0)"
 
 
-class M132(GCodeCommand):
-
+class M132(M130):
     def execute(self, g):
-        extr = g.get_int_by_letter("P", 0)
-        value = g.get_float_by_letter("S")
-        if extr == 0:
-            self.printer.heaters['E'].Td = value
-        elif extr == 1:
-            self.printer.heaters['H'].Td = value
-        elif extr == 2:
-            self.printer.heaters['HBP'].Td = value
+        self.get_params(g)
+        self.printer.heaters[M132.heaters[self.extr]].Td = self.value
 
     def get_description(self):
         return "Set PID D-value, Format (M132 P0 S8.0)"
