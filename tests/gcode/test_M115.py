fa31e421266062c8cf04f39fc658fa9968160f74
Richard Wackerbarth <rkw@dataplex.net>
2018-04-26 08:19:04 -0500

    Remove __release_name__
    The concept of naming versions of Redeem serves no
    useful purpose, creates a point of failure in
    maintaining proper version recognition, and creates
    non-compliant capability reports

--- tests/gcode/test_M115.py
+++ tests/gcode/test_M115.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import re
 from .MockPrinter import MockPrinter
 from redeem.Gcode import Gcode
+from redeem import __long_version__
 
 
 class M115_Tests(MockPrinter):
@@ -15,9 +16,8 @@ class M115_Tests(MockPrinter):
         self.assertRegexpMatches(g.answer, "PROTOCOL_VERSION:\S+")
         self.assertRegexpMatches(g.answer, "REPLICAPE_KEY:TESTING_DUMMY_KEY")
         self.assertRegexpMatches(g.answer, "FIRMWARE_NAME:Redeem")
-        self.assertRegexpMatches(g.answer, "FIRMWARE_VERSION:{}\s".format(re.escape(self.printer.firmware_version)))
+        self.assertRegexpMatches(g.answer, "FIRMWARE_VERSION:{}\s".format(re.escape( __long_version__)))
         self.assertRegexpMatches(g.answer, "FIRMWARE_URL:https?%3A\S+")
         self.assertRegexpMatches(g.answer, "MACHINE_TYPE:{}\s".format(
             re.escape(self.printer.config.get('System', 'machine_type'))))
         self.assertRegexpMatches(g.answer, "EXTRUDER_COUNT:{}".format(self.printer.NUM_AXES - 3))
-
