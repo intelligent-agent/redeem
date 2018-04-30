5554e31a946c21bb9a10058f027187278ae180a1
Troy <troy@thismuch.net>
2018-04-29 22:42:20 -0500

    These M-Codes need to be buffered, otherwise they occur out of sync with probe or other commands.

--- redeem/gcodes/M280.py
+++ redeem/gcodes/M280.py
@@ -40,6 +40,9 @@ class M280(GCodeCommand):
         else:
             logging.warning("M280: Servo index out of range "+str(index))
 
+    def is_buffered(self):
+        return True
+
     def get_description(self):
         return "Set servo position"
 
