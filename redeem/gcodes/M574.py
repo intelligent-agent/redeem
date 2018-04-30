02dc750bea77bcd3099af9811551443d0f2b86ea
Troy <troy@thismuch.net>
2018-04-29 22:42:20 -0500

    These M-Codes need to be buffered, otherwise they occur out of sync with probe or other commands.

--- redeem/gcodes/M574.py
+++ redeem/gcodes/M574.py
@@ -47,6 +47,9 @@ class M574(GCodeCommand):
             g.set_answer("ok "+", ".join([v.name+" stops: "+str(v.stops)+" " for _,v in sorted(iteritems(self.printer.end_stops))]))
 
 
+    def is_buffered(self):
+        return True
+
     def get_description(self):
         return "Set or get end stop config"
 
