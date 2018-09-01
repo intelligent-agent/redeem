379f07b18a6c5c6ad38419380bf74ebfc22803fd
Andrew Wiley <wiley@outlook.com>
2018-09-01 13:19:12 -0700

    add PathQueueIndex type to eliminate path index arithmetic bugs

--- redeem/path_planner/PathOptimizer.h
+++ redeem/path_planner/PathOptimizer.h
@@ -11,7 +11,7 @@ private:
     void calculateSafeSpeed(Path& path);
 
 public:
-    int64_t beforePathRemoval(std::vector<Path>& queue, size_t first, size_t last) override;
-    int64_t onPathAdded(std::vector<Path>& queue, size_t first, size_t last) override;
+    int64_t beforePathRemoval(std::vector<Path>& queue, PathQueueIndex first, PathQueueIndex last) override;
+    int64_t onPathAdded(std::vector<Path>& queue, PathQueueIndex first, PathQueueIndex last) override;
     void setMaxSpeedJumps(const VectorN& maxSpeedJumps);
 };
