e5e8cbc1895845812e6793e7f9379e82f8b37304
Richard Wackerbarth <rkw@dataplex.net>
2018-09-16 18:19:08 -0500

    Import redeem relative to file rather than cwd

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,4 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.realpath(os.path.dirname(__file__)),'..','redeem'))
 
--- tests/core/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '.')    # tests/ = testing root folder
-sys.path.insert(0, '..')    # tests/ = testing root folder
-sys.path.insert(0, '../redeem')
