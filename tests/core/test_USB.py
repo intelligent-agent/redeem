c18fac58bc13d4d9785a24c29dc610c70e2eb6ac
Richard Wackerbarth <rkw@dataplex.net>
2018-12-20 23:49:21 -0600

    Skip USB when testing

--- tests/core/test_USB.py
+++ tests/core/test_USB.py
@@ -1,11 +1,13 @@
 # Work in progress - ARC
-import unittest
 import mock    # use mock.Mock etc
-from USB import USB
+import pytest
+import unittest
 
 
 class TestUSB(unittest.TestCase):
+  @pytest.mark.skip
   def setUp(self):
+    from USB import USB
     testUSB = USB()
 
 
