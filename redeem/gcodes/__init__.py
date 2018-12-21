98af3a3d310d0daab8da1090495d82f55294cdde
Richard Wackerbarth <rkw@dataplex.net>
2018-12-20 23:51:42 -0600

    Suppress traceback when resolve is called twice

--- redeem/gcodes/__init__.py
+++ redeem/gcodes/__init__.py
@@ -4,7 +4,6 @@ import os
 
 #Import all python files as submodule
 for f in glob.glob(os.path.dirname(__file__) + "/*.py"):
-  if os.path.isfile(f) and not f.endswith('__init__.py') and \
-          not f.endswith('GCodeCommand.py'):
+  if os.path.isfile(f) and not f.endswith('__init__.py'):
     moduleName = os.path.splitext(os.path.basename(f))[0]
     importlib.import_module('.' + moduleName, 'redeem.gcodes')
