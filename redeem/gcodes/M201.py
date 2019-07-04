59171ed708bf86ed94a9c0007c5ac1f687a497ac
Elias Bakken <elias@iagent.no>
2019-07-04 21:30:25 +0200

    Changed M201 so accelleation is updated in the printer object first.

--- redeem/gcodes/M201.py
+++ redeem/gcodes/M201.py
@@ -14,23 +14,19 @@ from .GCodeCommand import GCodeCommand
 
 class M201(GCodeCommand):
   def execute(self, g):
-
-    t = []
     for i, axis in enumerate(self.printer.AXES):
       if g.has_letter(axis):
-        t.append(round(g.get_distance_by_letter(axis) / 3600.0, 4))
+        self.printer.acceleration[i] = round(g.get_distance_by_letter(axis) / 3600.0, 4)
 
     if self.printer.axis_config == self.printer.AXIS_CONFIG_CORE_XY or self.printer.axis_config == self.printer.AXIS_CONFIG_H_BELT:
       # x and y should have same accelerations for lines to be straight
-      t[1] = t[0]
+      self.printer.acceleration[1] = self.printer.acceleration[0]
     elif self.printer.axis_config == self.printer.AXIS_CONFIG_DELTA:
       # Delta should have same accelerations on all main axes
-      t[1] = t[0]
-      t[2] = t[0]
+      self.printer.acceleration[1] = self.printer.acceleration[0]
+      self.printer.acceleration[2] = self.printer.acceleration[0]
 
-    logging.debug("M201: acceleration = " + str(t))
-
-    self.printer.path_planner.native_planner.setAcceleration(t)
+    self.printer.path_planner.native_planner.setAcceleration(tuple(self.printer.acceleration))
 
   def get_description(self):
     return "Set print acceleration"
@@ -38,10 +34,10 @@ class M201(GCodeCommand):
   # todo: fix the description of the units.
   def get_long_description(self):
     return ("""
-Sets the acceleration that axes can do in units/minute^2 for print moves. 
+Sets the acceleration that axes can do in units/minute^2 for print moves.
 Example: M201 X1000 Y1000 Z100 E2000"
 
-Values get rounded to nearest whole number, in current G20/21 units. 
+Values get rounded to nearest whole number, in current G20/21 units.
 For CoreXY and HJ-belt mechines, Y value is forced to that supplied for X (Y is ignored).
 For Delta machines, X and Y values are forced to that supplied for X (Y and Z are ignored).
 In all cases, axes H, A, B and C remain independent.
